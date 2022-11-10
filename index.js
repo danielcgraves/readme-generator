bcef822c9a107b7e80901ef05fa5bac1683267fe
Daniel Graves <68198944+danielcgraves@users.noreply.github.com>
2022-11-10 11:43:26 -0700

    Got the generateMarkdown function working in the terminal

---
+++ index.js
@@ -0,0 +1,53 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    /* {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What needs to be installed for the project to run?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: '',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Who contributed to this project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Are there any instructions on how to test this project?',
+    }, */
+];
+
+inquirer.prompt(questions).then(({ title }) => {
+    console.log('title :>> ', title);
+
+    const markdown = generateMarkdown(title);
+    console.log(markdown);
+});
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
